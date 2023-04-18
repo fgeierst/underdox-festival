c3a9a71d749ed6d03b3e218075d954eaa372661b
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-04-18 22:18:12 +0200

    Feature: Subheader

---
+++ wp-content/themes/underdox-block-theme/patterns/subheader.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: subheader
+ * Slug: underdoxblocktheme/subheader
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"contentPosition":"center center","isDark":false} -->
+<div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:image {"align":"center","id":28,"width":372,"height":160,"sizeSlug":"full","linkDestination":"none","style":{"border":{"width":"0px","style":"none"}},"className":"is-style-default"} -->
+<figure class="wp-block-image aligncenter size-full is-resized has-custom-border is-style-default"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Group-8.png" alt="" class="wp-image-28" style="border-style:none;border-width:0px" width="372" height="160"/></figure>
+<!-- /wp:image --></div></div>
+<!-- /wp:cover -->
