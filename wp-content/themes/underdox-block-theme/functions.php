ad0fcfd0bbbf4db6d6fc7bfc94f98b0503fd3545
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-04-23 13:59:21 +0200

    Feat: use style.css also in editor

--- wp-content/themes/underdox-block-theme/functions.php
+++ wp-content/themes/underdox-block-theme/functions.php
@@ -1,6 +1,16 @@
 <?php
 
-function underdox_enqueue_style_sheet()
+// Examples
+// - https://github.com/andersnoren/abisko/blob/main/functions.php
+// - https://github.com/bgardner/powder/blob/main/functions.php
+
+function underdox_setup() {
+	add_theme_support( 'wp-block-styles' );
+	add_editor_style( get_template_directory_uri() . '/style.css' );
+}
+add_action( 'after_setup_theme', 'underdox_setup' );
+
+function underdox_styles()
 {
 	wp_enqueue_style(
 		'underdox',
@@ -9,7 +19,7 @@ function underdox_enqueue_style_sheet()
 		wp_get_theme('underdox-block-theme')->get('Version')
 	);
 }
-add_action('wp_enqueue_scripts', 'underdox_enqueue_style_sheet');
+add_action('wp_enqueue_scripts', 'underdox_styles');
 
 /* function underdox_block_theme_enqueue_block_assets() {
     wp_enqueue_script(
